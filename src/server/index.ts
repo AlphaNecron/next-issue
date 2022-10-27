407d26ce6e9e9b79d9d5a77f044c59b97514aa17
AlphaNecron <teddyowo@outlook.com>
2022-10-27 20:22:41 +0700

    initial

---
+++ src/server/index.ts
@@ -0,0 +1,24 @@
+import next from 'next';
+import type { NextServer } from 'next/dist/server/next';
+import { createServer } from 'http';
+
+export class Server {
+  private server: NextServer;
+  constructor() {
+    this.server = next({
+      hostname: '0.0.0.0',
+      port: 3000,
+      dir: '.',
+      quiet: false
+    });
+  }
+  start() {
+    this.server.prepare().then(() => {
+      const handler = this.server.getRequestHandler();
+      const server = createServer(handler);
+      server.listen(3000, '0.0.0.0', null, () => {
+        console.log('listening on :3000');
+      });
+    })
+  }
+}
